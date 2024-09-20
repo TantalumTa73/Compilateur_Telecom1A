36e722e9c9a4086d9a5bbe86c8baaa65973cbf5d
Yann <ruellan.yann@laposte.net>
2024-09-20 12:53:45 +0200

    Add the cmake file

--- Interpreter/main.cpp
+++ Interpreter/main.cpp
@@ -1,4 +1,10 @@
-#include <unordered_map> //to store variables
+#include <unordered_map> // dictionary
 #include <iostream>
 
 #include "json_reader.hpp"
+#include "token.hpp"
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
