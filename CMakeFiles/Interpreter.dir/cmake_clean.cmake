820230c8e54c3380c240303e4f736393bfe50ac9
Dadidou <luodaniel2015@yahoo.fr>
2024-09-23 09:20:44 +0200

    sh file added

---
+++ CMakeFiles/Interpreter.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Interpreter.dir/interpreter.cpp.o"
+  "CMakeFiles/Interpreter.dir/interpreter.cpp.o.d"
+  "CMakeFiles/Interpreter.dir/json_reader.cpp.o"
+  "CMakeFiles/Interpreter.dir/json_reader.cpp.o.d"
+  "CMakeFiles/Interpreter.dir/main.cpp.o"
+  "CMakeFiles/Interpreter.dir/main.cpp.o.d"
+  "CMakeFiles/Interpreter.dir/token.cpp.o"
+  "CMakeFiles/Interpreter.dir/token.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Interpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Interpreter.dir/interpreter.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/Interpreter.dir/interpreter.cpp.o: \
+  /Users/dadidou/Documents/Telecom/1A/Info/Compilation/Imperative_Compiler/Interpreter/interpreter.cpp
