981b36ec9e2ed6124227db08cf9960cb9e8a61ac
Yann <ruellan.yann@laposte.net>
2024-09-24 15:07:52 +0200

    Refactor interpret.sh script to log build and execution steps

--- interpret.sh
+++ interpret.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 _cfilename="${1:-file.c}"
-dune build --root ParserLexer
-./ParserLexer/expr2json.exe "$_cfilename"
-cmake Interpreter -B Interpreter/build
-make -C Interpreter/build/
-./Interpreter/build/main "${_cfilename%".c"}.json"
+dune build --root ParserLexer >> log.txt
+./ParserLexer/expr2json.exe "$_cfilename"  >> log.txt
+cmake Interpreter -B Interpreter/build  >> log.txt
+make -C Interpreter/build/  >> log.txt
+y="${_cfilename%".c"}.json"
+echo "y is $y"
+./Interpreter/build/main $y
