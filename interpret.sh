5ae753fda1c9f8a3fae444bdf355d9e78ce873d4
Yann <ruellan.yann@laposte.net>
2024-09-24 15:13:28 +0200

    update interpret

--- interpret.sh
+++ interpret.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 _cfilename="${1:-file.c}"
+_jsonfilename="${_cfilename%".c"}.json"
 dune build --root ParserLexer >> log.txt
 ./ParserLexer/expr2json.exe "$_cfilename"  >> log.txt
 cmake Interpreter -B Interpreter/build  >> log.txt
 make -C Interpreter/build/  >> log.txt
-y="${_cfilename%".c"}.json"
-echo "y is $y"
-./Interpreter/build/main $y
+echo "c file    : $_cfilename"  >> log.txt
+echo "json file : $_jsonfilename"  >> log.txt
+./Interpreter/build/main $_jsonfilename
