5d30e511bf6f07762d257780dafa8813a701735a
Yann <ruellan.yann@laposte.net>
2024-09-25 09:42:44 +0200

    Add compile-cpp.sh and compile.sh scripts

---
+++ Bash/compile-cpp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+_cfilename="${1:-file.c}"
+_jsonfilename="${_cfilename%".c"}.json"
+_sfilename="${_cfilename%".c"}.s"
+_exefilename="${_cfilename%".c"}"
+
+
+echo ""  >> log.txt
+echo "------------------------------------"  >> log.txt
+echo "c file    : $_cfilename"  >> log.txt
+echo "json file : $_jsonfilename"  >> log.txt
+
+./Bash/build-dune.sh
+./Bash/build-compiler.sh
+
+
+./ParserLexer/expr2json.exe "$_cfilename"  >> log.txt
+./Compiler/build/main $_jsonfilename >> log.txt
+gcc $_sfilename -o $_exefilename
+./$_exefilename
