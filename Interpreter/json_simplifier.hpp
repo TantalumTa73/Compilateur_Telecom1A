d1485c1858ec689018ca37ddf34d0886e7042bec
Yann <ruellan.yann@laposte.net>
2024-09-30 18:07:09 +0200

    Refactor code to remove json_simplifier module

--- Interpreter/json_simplifier.hpp
+++
@@ -1,7 +0,0 @@
-#include <vector>
-#include <string>
-
-#include "token.hpp"
-
-Token simplify(Token token);
-std::vector<Token> simplify(std::vector<Token> token);
--- Interpreter/simplified_token.cpp
+++ Interpreter/simplified_token.cpp
@@ -2,23 +2,31 @@
 #include <string>
 
 #include "token.hpp"
-#include "json_simplifier.hpp"
+#include "simplified_token.hpp"
 
-Token simplify(Token token){
-    Token new_token = Token(token.start_line, token.start_char, token.end_line, token.end_char);
+SimplifiedToken simplify(Token token){
+    // SimplifiedToken new_token ;
 
-    new_token.childs = simplify(token.childs) ;
-
-    for (auto const& [key, val] : token.attributes){
-        new_token.set_attribute(key, val);
+    if (token.get_attribute("action") == "gfundef"){
+        return FunctionDef(token, token.get_attribute("name"), token.get_attribute("arg"), simplify(token.childs));
+    } else if (token.get_attribute("action") == "gvardef"){
+        return VarDef(token, token.get_attribute("name"));
+    } else if (token.get_attribute("action") == "varset"){
+        return VarSet(token, token.get_attribute("name"));
+    } else if (token.get_attribute("action") == "function"){
+        return FunctionCall(token, token.get_attribute("name"), token.childs[0].get_attribute("name"));
+    } else if (token.get_attribute("action") == "calc"){
+        return Calc(token, token.get_attribute("type"), token.childs.size());
+    } else if (token.get_attribute("action") == "return"){
+        return Return(token);
     }
 
-    return new_token;
+    throw "Unknown action";
 }
 
-std::vector<Token> simplify(std::vector<Token> tokens){
+std::vector<SimplifiedToken> simplify(std::vector<Token> tokens){
 
-    std::vector<Token> new_tokens = std::vector<Token>();
+    std::vector<SimplifiedToken> new_tokens = std::vector<SimplifiedToken>();
     reverse(tokens.begin(), tokens.end()) ;
    
 
