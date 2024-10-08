c266a9a39562e5d9032b0f417b4cfe233eb9d705
Yann <ruellan.yann@laposte.net>
2024-10-08 14:21:14 +0200

    Switch on old version (sould now work)

--- compile.sh
+++ compile.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 # ./Bash/interpret-cpp.sh $1 $2
-./Bash/compile-py.sh $1
+
+# ./Bash/compile-py.sh $1
+
+_cfilename="${1:-file.c}"
+cd ParserLexer
+eval $(opam env)
+dune build
+cd ..
+cp ParserLexer/expr2json.exe .
+./expr2json.exe "$_cfilename"
+python3 PythonCompiler/compiler.py ${_cfilename%".c"}.json PythonCompiler/template.s
