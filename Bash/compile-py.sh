0797728596c238f038686617a61fb7d29fa7bbc1
Yann <ruellan.yann@laposte.net>
2024-10-08 13:44:08 +0200

    Correct mistake (sould really compile)

--- Bash/compile-py.sh
+++ Bash/compile-py.sh
@@ -3,6 +3,6 @@
 _cfilename="${1:-file.c}"
 
 ./Bash/build-dune.sh
-./Bash/parse $_cfilename
+./Bash/parse.sh $_cfilename
 
 python3 PythonCompiler/compiler.py ${_cfilename%".c"}.json PythonCompiler/template.s
