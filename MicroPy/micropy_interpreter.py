34fb31a2c97e2f3e51e53264aea9befe1191bb6e
Pacome Fromager <45541358+Teyzer@users.noreply.github.com>
2024-10-09 10:19:43 +0200

    support de fonctions v0.1

--- MicroPy/micropy_interpreter.py
+++ MicroPy/micropy_interpreter.py
@@ -32,46 +32,48 @@ class Variable:
 
 
 
-def get_variable_object(data, scope: int = 0, to_be_modified = False):
+def get_variable_object(data, depth: int = 0, to_be_modified = False):
 
     # print(data)
     
     if data["type"] == "var":
         
         varname = data["name"]
-        if scope < len(cur_vars) and varname in cur_vars[scope]:
-            return cur_vars[scope][varname] 
+        if depth < len(cur_vars) and varname in cur_vars[depth]:
+            return cur_vars[depth][varname] 
         elif varname in cur_vars[0] and (not to_be_modified or cur_vars[0][varname].can_be_modified):
             return cur_vars[0][varname]
         else:
-            cur_vars[scope][varname] = Variable(None)
-            return cur_vars[scope][varname]
+            cur_vars[depth][varname] = Variable(None)
+            return cur_vars[depth][varname]
 
 
 
-# def set_variable(data, value, scope: int = 0):
+# def set_variable(data, value, depth: int = 0):
 
 #     if data["type"] == "array access":
 #         return 
 
-#     if scope < len(current_variables) and name in current_variables[scope]:
-#         current_variables[scope][name].vset(value)
+#     if depth < len(current_variables) and name in current_variables[depth]:
+#         current_variables[depth][name].vset(value)
 #     elif name in current_variables[0] and current_variables[0][name].can_be_modified:
 #         current_variables[0][name].vset(value)
 #     else:
-#         current_variables[scope][name] = Variable(value)
+#         current_variables[depth][name] = Variable(value)
 
 
-# def get_variable(name: int, scope: int = 0):
+# def get_variable(name: int, depth: int = 0):
 
-#     if scope < len(current_variables) and name in current_variables[scope]:
-#         current_variables[scope][name].vget()
+#     if depth < len(current_variables) and name in current_variables[depth]:
+#         current_variables[depth][name].vget()
 #     elif name in current_variables[0]:
 #         current_variables[0][name].vget()
 
 
 
-def evaluate_expression(expr):
+def evaluate_expression(expr, depth: int = 0):
+
+    # print(expr, depth)
 
     operators = {
         "Add": lambda x, y: x + y,
@@ -81,30 +83,33 @@ def evaluate_expression(expr):
     }
 
     if expr["type"] == "binop":
-        v1 = evaluate_expression(expr["v1"])
-        v2 = evaluate_expression(expr["v2"])
+        v1 = evaluate_expression(expr["v1"], depth)
+        v2 = evaluate_expression(expr["v2"], depth)
         return operators[expr["binop"]](v1, v2)
     
     if expr["type"] == "const":
-        return evaluate_expression(expr["value"])
+        return evaluate_expression(expr["value"], depth)
     
     if expr["type"] == "int":
         return expr["value"]
     
     if expr["type"] == "list":
-        return [evaluate_expression(x) for x in expr["content"]]
+        return [evaluate_expression(x, depth) for x in expr["content"]]
 
     if expr["type"] == "var":
         # print("aaaaaaa", expr)
-        return get_variable_object(expr).vget()
+        return get_variable_object(expr, depth).vget()
     
     if expr["type"] == "left_value":
-        return evaluate_expression(expr["value"])
+        return evaluate_expression(expr["value"], depth)
     
     if expr["type"] == "array access":
-        # print(expr)
         array_object = get_variable_object(expr["array"])
-        return array_object.vget()[evaluate_expression(expr["index"])]
+        return array_object.vget()[evaluate_expression(expr["index"], depth)]
+
+    if expr["type"] == "call":
+        args = [evaluate_expression(x) for x in expr["args"]]
+        return evaluate_function(expr["funname"], args, depth + 1)
 
 
 def evaluate(line, depth: int = 0):
@@ -119,13 +124,13 @@ def evaluate(line, depth: int = 0):
     if line["type"] == "varset":
         # print(line.keys())
         var = get_variable_object(line["left_value"], depth, True)
-        value = evaluate_expression(line["value"])
+        value = evaluate_expression(line["value"], depth)
         var.vset(value)
         # set_variable(line["name"], evaluate_expression(line["value"]))
         return
     
     if line["type"] == "expr":
-        return evaluate(line["value"])
+        return evaluate(line["value"], depth)
     
     if line["type"] == "call":
 
@@ -133,11 +138,43 @@ def evaluate(line, depth: int = 0):
         # print("6+++++++++++++++++++", line)
 
         if line["funname"] == "print":
-            print(*[evaluate_expression(x) for x in line["args"]])
+            print(*[evaluate_expression(x, depth) for x in line["args"]])
             return
 
         # return evaluate_expression(line["expr"])
     
+
+def evaluate_function(name, args, depth: int = 0):
+
+    # print(cur_vars)
+    cur_vars.append({})
+    # print(cur_vars)
+    # print(depth)
+
+
+    arg_names, func_json = cur_funcs[name]
+    for arg_name, arg_value in zip(arg_names, args):
+        cur_vars[depth][arg_name] = Variable(arg_value)
+
+    # print(arg_names, func_json)
+
+    for line in func_json["body"]:
+        # print(line)
+        # print(cur_vars)
+
+        if line["type"] == "return":
+            return evaluate_expression(line["value"], depth)
+
+        evaluate(line, depth)
+
+    # _, body = cur_funcs[name]
+    # for arg in args:
+    #     cur_vars[depth][arg] = Variable(None)
+    # for line in body:
+    #     evaluate(line, depth)
+
+    cur_vars.pop()
+
 
 
 
---
+++ TestFiles/little.py
@@ -0,0 +1,11 @@
+a = [7, 2]
+print(a)
+
+a = 3
+print(a)
+
+def a(n):
+    n = n + 1
+    return n
+
+print(a(2))
