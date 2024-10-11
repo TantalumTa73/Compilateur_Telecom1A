6ebbebeea632fd1b1394e50fc6ad191bae50cc02
Yann <ruellan.yann@laposte.net>
2024-10-11 17:34:31 +0200

    Move python test files and bash files

--- Bash/build-micropy.sh
+++ Bash/build-micropy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake InterpMicroPyCppreter -B MicroPyCpp/build  >> log.txt
+cmake MicroPyCpp -B MicroPyCpp/build  >> log.txt
 make -C MicroPyCpp/build/  >> log.txt
--- Bash/ptipython-cpp.sh
+++ Bash/ptipython-cpp.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 py_file="${1:-file.py}"
-json_file="${c_file%".py"}.json"
+json_file="${py_file%".py"}.json"
+
+echo "Compiling $py_file to $json_file"
 
 ./Bash/build-dune-py.sh 
 ./Bash/build-micropy.sh
 
-./ParserPy/ptipython2json.exe "$py_file"  >> log.txt
+# ./ParserPy/ptipython2json.exe "$py_file"  >> log.txt
 ./MicroPyCpp/build/main $json_file $2
