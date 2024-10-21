37295a18d3b938b68f1ab7b4cab07b3a491d967e
Yann <ruellan.yann@laposte.net>
2024-10-21 09:52:39 +0200

    Compile : bad file

--- Bash/compile-py.sh
+++ Bash/compile-py.sh
@@ -6,7 +6,7 @@ x86_file="${c_file%".c"}.s"
 
 echo "Compiling $c_file to $x86_file (json: $json_file)"
 
-./Bash/build-dune.sh
+./Bash/build-dune-c.sh
 
 ./ParserLexer/expr2json.exe $c_file
 python3 PythonCompiler/compiler.py $json_file "PythonCompiler/template.s"
