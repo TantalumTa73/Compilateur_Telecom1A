a5a60be44c24a10684928351b36a2569e2fae6f3
Yann <ruellan.yann@laposte.net>
2024-11-03 23:02:13 +0100

    Try correct .sh file for finalproject-py

--- Bash/finalproject-py.sh
+++ Bash/finalproject-py.sh
@@ -17,7 +17,21 @@ echo "json file : $json_file"  >> log.txt
 
 
 ./ParserBetter/expr2json.exe "$c_file"  >> log.txt
-python3 FinalProjectPy/core.py $json_file "FinalProjectPy/template.s"
+# python3 FinalProjectPy/core.py $json_file "FinalProjectPy/template.s"
+python3 FinalProjectPy/core.py $json_file "PythonCompiler/template.s"
 # cd ..
-gcc $s_file -o $out_file
-./$out_file
+# gcc $s_file -o $out_file
+# ./$out_file
+# You just need to compute the .s file, not to run it
+
+# The error when I try your compiler :
+# python3 FinalProjectPy/core.py TestFinal/variable.json "PythonCompiler/template.s"
+# {'action': 'vardef', 'type': 'int', 'name': 'a', 'size': [], 'value': {'action': 'litteral', 'value': {'action': 'int', 'value': 0}}}
+# Traceback (most recent call last):
+#   File "/cal/exterieurs/yruellan-24/IMP_comp/imperative_compiler/FinalProjectPy/core.py", line 576, in <module>
+#     define_function(element["name"], element["type"], element["args"], scope, appened)
+#   File "/cal/exterieurs/yruellan-24/IMP_comp/imperative_compiler/FinalProjectPy/core.py", line 432, in define_function
+#     evaluate_scope(scope["body"], funcname, return_type, current_depth)
+#   File "/cal/exterieurs/yruellan-24/IMP_comp/imperative_compiler/FinalProjectPy/core.py", line 213, in evaluate_scope
+#     VARIABLE_OFFSET += memory_var_size
+# TypeError: unsupported operand type(s) for +=: 'int' and 'list'
