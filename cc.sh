7a8c6ddf7b97e366dd0dac516dbe7cba6d4e688c
Yann <ruellan.yann@laposte.net>
2024-11-05 17:01:23 +0100

    Delete HACK

--- cc.sh
+++ cc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-touch "_temp.c"
-echo "#include <stdlib.h>" > "_temp.c"
-echo "#include <stdio.h>" >> "_temp.c"
-echo "void print_int(int x){ printf(\"%d\n\",x);}" >> "_temp.c"
-cat $1 >> "_temp.c"
-gcc -S -O3 "_temp.c" -o file.s
-# ./Bash/finalproject-cpp.sh $1 $2
+# touch "_temp.c"
+# echo "#include <stdlib.h>" > "_temp.c"
+# echo "#include <stdio.h>" >> "_temp.c"
+# echo "void print_int(int x){ printf(\"%d\n\",x);}" >> "_temp.c"
+# cat $1 >> "_temp.c"
+# gcc -S -O3 "_temp.c" -o file.s
+./Bash/finalproject-cpp.sh $1 $2
 # ./Bash/finalproject-py.sh $1
