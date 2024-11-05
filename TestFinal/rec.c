96f18c92eca3b0bf8a3b577a10865a2f6b5327b2
Yann <ruellan.yann@laposte.net>
2024-11-05 15:53:29 +0100

    Fix bool sys.

--- TestFinal/rec.c
+++ TestFinal/rec.c
@@ -1,17 +1,16 @@
 #include <stdbool.h>
 
-int factorial(int n, bool a) {
-    if (n == 0) {
-        if (a) {
-            return 1;
-        }
-        return 2;
+int factorial(int n, int a) {
+    
+    if (n <= 0) {
+        return 1;
     } else {
-        return n * factorial(n - 1, a);
+        return n*factorial(n-1, a);
     }
 }
 
 
 int main() {
-    print_int(factorial(10, 1));
+    int x = factorial(10, 1) ;
+    print_int(x);
 }
