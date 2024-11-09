23e08a2e925689486ff5200a1598b25c1134dc96
Pacome Fromager <45541358+Teyzer@users.noreply.github.com>
2024-11-09 14:30:22 +0100

    switch to python

--- TestFinal/array.c
+++ TestFinal/array.c
@@ -1,10 +1,11 @@
-int tab[100];
-
 int main(){
-    for (int i = 0; i < 100; i ++){
+
+    int tab[100];
+
+    for (int i = 0; i < 100; i = i + 1){
         tab[i] = i;
     }
-    for (int i = 0; i < 100; i ++){
+    for (int i = 0; i < 100; i = i + 1){
         print_int(tab[i]);
     }
     return 0;
